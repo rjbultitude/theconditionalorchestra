b03994072951003494c17af7f39ca4d41e681cc8
rjbultitude <richard.bultitude@gmail.com>
2016-08-01 22:27:57 +0100

    remove duplicate IDs in SVGs. Use pubsub to ensure we only paint and draw shapes when the dialog is open

--- src/scripts/modules/visuals-dialog.js
+++ src/scripts/modules/visuals-dialog.js
@@ -1,5 +1,7 @@
 'use strict';
 var A11yDialog = require('a11y-dialog');
+var postal = require('postal');
+var channel = postal.channel();
 
 module.exports = function() {
   // Get the dialog element (with the accessor method you want)
@@ -7,4 +9,11 @@ module.exports = function() {
 
   // Instanciate a new A11yDialog module
   var dialog = new A11yDialog(dialogEl);
+
+  dialogEl.addEventListener('dialog:show', function (e) {
+    channel.publish('dialogOpen', e.target);
+  });
+  dialogEl.addEventListener('dialog:hide', function (e) {
+    channel.publish('dialogClosed', e.target);
+  });
 };
