80d1e09e6b4015e4fa1aa1a860dfd73a5c98521d
Richard Bultitude <rbultitude@thisiszone.com>
2016-11-16 16:09:25 +0000

    add missing module to repo

---
+++ src/scripts/utilities/largest-num-in-array.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * [exports a function that inserts an array into another at any given index]
+ * @param  {[Array]} destArray     [the array to act on]
+ * @param  {[Number]} pos          [the index to start at]
+ * @param  {[Array]} arrayToInsert [the array to insert]
+ * @return {[Array]}               [the returned array]
+ */
+module.exports = function getLargestNumInArr(arr) {
+  return arr.reduce(function(prevVal, curVal) {
+    var _largest;
+    if (curVal > prevVal) {
+      _largest = curVal;
+    }
+    return _largest;
+  }, 0);
+};
