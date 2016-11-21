c811657a226eb29f96e6960187870fe4142ba90a
rjbultitude <richard.bultitude@gmail.com>
2016-11-21 14:56:04 +0000

    add new and renamed module

---
+++ src/scripts/utilities/largest-pos-num-in-array.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * returns the largest positive number in a given array
+ * @param  {[Array]} destArray     [the array to act on]
+ * @param  {[Number]} pos          [the index to start at]
+ * @param  {[Array]} arrayToInsert [the array to insert]
+ * @return {[Array]}               [the returned array]
+ */
+module.exports = function getLargestPosNumInArr(arr) {
+  return arr.reduce(function(prevVal, curVal) {
+    if (curVal > prevVal) {
+      prevVal = curVal;
+    }
+    return prevVal;
+  }, 0);
+};
