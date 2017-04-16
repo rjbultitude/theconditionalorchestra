d2f53516c890143ef167ced4799b95fe66568295
rjbultitude <richard.bultitude@gmail.com>
2017-04-16 21:25:12 +0100

    rename var in addMissingItems to avoid confusion with global objects

--- src/scripts/utilities/add-missing-array-items.js
+++ src/scripts/utilities/add-missing-array-items.js
@@ -4,27 +4,25 @@
  * Duplicates items 'difference' number of times
  * and can add a given amount to each duplicated item if desired
  * and can repeat as many times as specified
- * @param  {Array} array        [array to duplicate]
+ * @param  {Array} origArray        [origArray to duplicate]
  * @param  {Number} difference  [number of items to duplicate]
  * @param  {Number} amountToAdd  [numeric value to add to each duplicated item]
  * @param  {Number} repeat  [number of times to repeat the whole process]
- * @return {Array}          [array]
+ * @return {Array}          [origArray]
  */
-module.exports = function addMissingArrayItems(array, difference, amountToAdd, repeat) {
+module.exports = function addMissingArrayItems(origArray, difference, amountToAdd, repeat) {
   //error check
   if (typeof difference !== 'number') {
     console.error('difference is not a number');
   }
   var _index = 0;
-  var _newArr = array.map(function(item) {
+  var _newArr = origArray.map(function(item) {
     return item;
   });
   var _finalArr = [];
   var _diffArr = [];
   var _newVal;
-  //TODO is this correct?
-  //shouldnt it be (array.length -1) * repeat;
-  var _repeatPoint = (array.length * repeat) - 1;
+  var _repeatPoint = (origArray.length * repeat) - 1;
   console.log('adding ' + difference + ' number of missing items');
   // loop the number of times
   // needed to make the missing items
@@ -43,7 +41,7 @@ module.exports = function addMissingArrayItems(array, difference, amountToAdd, r
       _index = 0;
       amountToAdd = 0;
       continue addMissingLoop;
-    } else if (_index === array.length - 1) {
+    } else if (_index === origArray.length - 1) {
       _index = 0;
       amountToAdd += amountToAdd;
       continue addMissingLoop;
