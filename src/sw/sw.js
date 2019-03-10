2ea2d5b56283f85dc93fc3243d177e54484df6c4
rjbultitude <richard.bultitude@gmail.com>
2019-03-10 14:44:29 +0000

    increment sw caches. add codacy config

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v4';
-var CACHE_DYNAMIC_NAME = 'dynamic-v3';
+var CACHE_STATIC_NAME = 'static-v5';
+var CACHE_DYNAMIC_NAME = 'dynamic-v4';
 
 self.isOnlyIfCached = function isOnlyIfCached(event) {
   if (event.request !== undefined) {
