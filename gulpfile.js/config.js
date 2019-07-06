0e0eb3c6c8ae719d0d383ba7fc96ad6169f0a2f7
rjbultitude <richard.bultitude@gmail.com>
2019-07-06 13:31:11 +0100

    autoprefixer api updates

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -4,15 +4,6 @@ var path = require('path');
 var util = require('gulp-util');
 
 module.exports = {
-    autoprefixer: {
-        browsers: [
-            'last 2 versions',
-            'Android 4',
-            'IE 8',
-            'IE 9',
-            'iOS >= 6'
-        ]
-    },
     production: !!util.env.production,
     sass: {
         errLogToConsole: true
