e806ed899686e422477e479912a3eb131cbbbd0e
rjbultitude <richard.bultitude@gmail.com>
2019-08-26 11:42:10 +0100

    add fahrenheit to celcius spec

---
+++ tests/frnht-to-celcius.spec.js
@@ -0,0 +1,19 @@
+var mocha = require('mocha');
+var expect = require('chai').expect;
+var frnhtToCelcius = require('../src/scripts/utilities/frnht-to-celcius.js');
+
+describe('fahrenheit to celcius', function() {
+  this.beforeAll(function() {
+    // zero degrees Celcius
+    this.tempInFahrenheit = 32;
+  });
+  it('should return a floating point number with number as argument', function() {
+    expect(frnhtToCelcius(50)).to.be.a('number');
+  });
+  it('should return zero with 32 degrees fahrenheit as argument', function() {
+    expect(frnhtToCelcius(this.tempInFahrenheit)).to.equal(0);
+  });
+  it('should return 10 with 50 degrees fahrenheit as argument', function() {
+    expect(frnhtToCelcius(50)).to.equal(10);
+  });
+});
